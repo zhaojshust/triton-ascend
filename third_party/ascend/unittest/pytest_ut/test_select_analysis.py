b965f1fa9e03b743b251a8b13fa38a9f19be18e7
wutianyao <wutianyao1@huawei.com>
2026-04-23 11:33:37 +0800

    testing: Add select analysis test cases
    Co-authored-by: wutianyao<wutianyao1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1452 merge main4 into main
    
    testing: Add select analysis test cases
    
    Created-by: wutianyao
    Commit-by: wutianyao
    Merged-by: ascend-robot
    Description: The core Triton is a small number of people, and we receive many PRs (thank
    you!).  To help us review your code more quickly, **if you are a new
    contributor (less than 3 PRs merged) we ask that you complete the following
    tasks and include the filled-out checklist in your PR description.**
    
    Complete the following tasks before sending your PR, and replace `[ ]` with
    `[x]` to indicate you have done them.
    
    - [ ] I am not making a trivial change, such as fixing a typo in a comment.
    
    - [ ] I have written a PR description following these
      [rules](https://cbea.ms/git-commit/#why-not-how).
    
    - [ ] I have run `pre-commit run --from-ref origin/main --to-ref HEAD`.
    
    - Select one of the following.
      - [ ] I have added tests.
        - `/test` for `lit` tests
        - `/unittest` for C++ tests
        - `/python/test` for end-to-end tests
      - [ ] This PR does not need a test because `FILL THIS IN`.
    
    - Select one of the following.
      - [ ] I have not added any `lit` tests.
      - [ ] The `lit` tests I have added follow these [best practices](https://mlir.llvm.org/getting_started/TestingGuide/#filecheck-best-practices),
        including the "tests should be minimal" section. (Usually running Python code
        and using the instructions it generates is not minimal.)
    
    
    See merge request: Ascend/triton-ascend!1452

--- third_party/ascend/unittest/pytest_ut/test_select_analysis.py
+++ third_party/ascend/unittest/pytest_ut/test_select_analysis.py
@@ -34,16 +34,194 @@ def kernel_cal_select_mask_bool(
     BLOCK: tl.constexpr,
 ):
     offs = tl.arange(0, BLOCK)
-    indice = tl.load(Indices_ptr)
+    index = tl.load(Indices_ptr)
 
     true_tensor = tl.arange(0, BLOCK) < numel
     false_tensor = tl.arange(0, BLOCK) >= numel
-    mask = offs < indice
+    mask = offs < index
     res = tl.where(mask, true_tensor, false_tensor)
     tl.store(
         Output_ptr + offs,
         res
     )
+
+
+@triton.jit
+def kernel_cal_select_mask_one_dim_static_offset(
+    QK_ptr,
+    Other_ptr,
+    Output_ptr,
+    BLOCK: tl.constexpr,
+    start_zero: tl.constexpr
+):
+    offs = tl.arange(0, BLOCK)
+
+    qk_ub = tl.load(
+        QK_ptr + offs
+    )
+    other = tl.load(
+        Other_ptr + offs
+    )
+
+    if start_zero:
+        mask = offs < 1
+    else:
+        mask = offs >= 1
+
+    res = tl.where(mask, qk_ub, other)
+    tl.store(
+        Output_ptr + offs,
+        res
+    )
+
+
+@triton.jit
+def kernel_cal_select_mask_one_dim_dyn_offset(
+    QK_ptr,
+    Other_ptr,
+    Output_ptr,
+    Indices_ptr,
+    BLOCK: tl.constexpr,
+    start_zero: tl.constexpr
+):
+    offs = tl.arange(0, BLOCK)
+    index = tl.load(Indices_ptr)
+
+    qk_ub = tl.load(
+        QK_ptr + offs
+    )
+    other = tl.load(
+        Other_ptr + offs
+    )
+
+    if start_zero:
+        mask = offs < index
+    else:
+        mask = offs >= index
+
+    res = tl.where(mask, qk_ub, other)
+    tl.store(
+        Output_ptr + offs,
+        res
+    )
+
+
+@triton.jit
+def kernel_cal_select_mask_one_dim_two_dyn_offset(
+    QK_ptr,
+    Other_ptr,
+    Output_ptr,
+    Indices_ptr,
+    BLOCK: tl.constexpr,
+):
+    offs = tl.arange(0, BLOCK)
+    minIndex = tl.load(Indices_ptr)
+    maxIndex = tl.load(Indices_ptr + 1)
+
+    qk_ub = tl.load(
+        QK_ptr + offs
+    )
+    other = tl.load(
+        Other_ptr + offs
+    )
+
+    mask = (offs < maxIndex) and (offs >= minIndex)
+    res = tl.where(mask, qk_ub, other)
+    tl.store(
+        Output_ptr + offs,
+        res
+    )
+
+
+@triton.jit
+def kernel_cal_select_mask_two_dim_static_offset(
+    QK_ptr,
+    Other_ptr,
+    Output_ptr,
+    BLOCK: tl.constexpr,
+    start_zero: tl.constexpr,
+):
+    rows = tl.arange(0, BLOCK)
+    cols = tl.arange(0, BLOCK)
+    offs = rows[:, None] * BLOCK + cols[None, :]
+
+    qk_ub = tl.load(
+        QK_ptr + offs
+    )
+    other = tl.load(
+        Other_ptr + offs
+    )
+    if start_zero: 
+        mask_rows = rows < 1
+        mask_cols = cols < 1
+    else:
+        mask_rows = rows >= 1
+        mask_cols = cols >= 1
+
+    res = tl.where(mask_rows[:, None] & mask_cols[None, :], qk_ub, other)
+    tl.store(Output_ptr + offs, res)
+
+
+@triton.jit
+def kernel_cal_select_mask_two_dim_one_dyn_offset(
+    QK_ptr,
+    Other_ptr,
+    Output_ptr,
+    Indices_ptr,
+    BLOCK: tl.constexpr,
+    start_zero: tl.constexpr,
+):
+    rows = tl.arange(0, BLOCK)
+    cols = tl.arange(0, BLOCK)
+    offs = rows[:, None] * BLOCK + cols[None, :]
+    minIndex = tl.load(Indices_ptr)
+    maxIndex = tl.load(Indices_ptr + 1)
+
+    qk_ub = tl.load(
+        QK_ptr + offs
+    )
+    other = tl.load(
+        Other_ptr + offs
+    )
+    if start_zero: 
+        mask_rows = rows < 1
+    else:
+        mask_rows = rows >= 1
+
+    mask_cols = (cols < maxIndex) & (cols >= minIndex)
+
+    res = tl.where(mask_rows[:, None] & mask_cols[None, :], qk_ub, other)
+    tl.store(Output_ptr + offs, res)
+
+
+@triton.jit
+def kernel_cal_select_mask_two_dim_all_dyn_offset(
+    QK_ptr,
+    Other_ptr,
+    Output_ptr,
+    Indices_ptr,
+    BLOCK: tl.constexpr,
+):
+    rows = tl.arange(0, BLOCK)
+    cols = tl.arange(0, BLOCK)
+    offs = rows[:, None] * BLOCK + cols[None, :]
+    min_index1 = tl.load(Indices_ptr)
+    max_index1 = tl.load(Indices_ptr + 1)
+    min_index2 = tl.load(Indices_ptr + 2)
+    max_index2 = tl.load(Indices_ptr + 3)
+
+    qk_ub = tl.load(
+        QK_ptr + offs
+    )
+    other = tl.load(
+        Other_ptr + offs
+    )
+
+    mask_rows = (rows < max_index1) & (rows >= min_index1)
+    mask_cols = (cols < max_index2) & (cols >= min_index2)
+
+    res = tl.where(mask_rows[:, None] & mask_cols[None, :], qk_ub, other)
+    tl.store(Output_ptr + offs, res)
 
 
 @triton.jit
@@ -79,18 +257,105 @@ def kernel_cal_select_mask(
 
 
 def torch_cal_select_mask_bool(
-    Indice: torch.Tensor,
+    index: torch.Tensor,
     numel,
     BLOCK,
 ):
     offs = torch.arange(0, BLOCK)
     true_tensor = torch.arange(0, BLOCK) < numel
     false_tensor = torch.arange(0, BLOCK) >= numel
-    mask = offs < Indice
+    mask = offs < index
 
     res = torch.where(mask, true_tensor, false_tensor)
     return res
-    
+
+
+def torch_cal_select_mask_one_dim_static_offset(
+    QK: torch.Tensor,
+    Other: torch.Tensor,
+    start_zero,
+):
+    Output = Other.clone()
+    if start_zero:
+        Output[:1] = QK[:1]
+    else:
+        Output[1:] = QK[1:]
+    return Output
+
+
+def torch_cal_select_mask_one_dim_dyn_offset(
+    QK: torch.Tensor,
+    Other: torch.Tensor,
+    Indices: torch.Tensor,
+    start_zero,
+):
+    limit_idx = Indices[0].item()
+    Output = Other.clone()
+    if start_zero:
+        Output[:limit_idx] = QK[:limit_idx]
+    else:
+        Output[limit_idx:] = QK[limit_idx:]
+    return Output
+
+
+def torch_cal_select_mask_one_dim_two_dyn_offset(
+    QK: torch.Tensor,
+    Other: torch.Tensor,
+    Indices: torch.Tensor,
+):
+    min_idx = Indices[0].item()
+    max_idx = Indices[1].item()
+    Output = Other.clone()
+    Output[min_idx:max_idx] = QK[min_idx:max_idx]
+
+    return Output
+
+
+def torch_cal_select_mask_two_dim_static_offset(
+    QK: torch.Tensor,
+    Other: torch.Tensor,
+    start_zero,
+):
+    Output = Other.clone()
+    if start_zero:
+        Output[:1, :1] = QK[:1, :1]
+    else:
+        Output[1:, 1:] = QK[1:, 1:]
+
+    return Output
+
+
+def torch_cal_select_mask_two_dim_one_dyn_offset(
+    QK: torch.Tensor,
+    Other: torch.Tensor,
+    Indices: torch.Tensor,
+    start_zero,
+):
+    min_idx = Indices[0].item()
+    max_idx = Indices[1].item()
+    Output = Other.clone()
+    if start_zero:
+        Output[:1, min_idx:max_idx] = QK[:1, min_idx:max_idx]
+    else:
+        Output[1:, min_idx:max_idx] = QK[1:, min_idx:max_idx]
+
+    return Output
+
+
+def torch_cal_select_mask_two_dim_all_dyn_offset(
+    QK: torch.Tensor,
+    Other: torch.Tensor,
+    Indices: torch.Tensor,
+):
+    min_idx1 = Indices[0].item()
+    max_idx1 = Indices[1].item()
+    min_idx2 = Indices[2].item()
+    max_idx2 = Indices[3].item()
+    Output = Other.clone()
+    Output[min_idx1: max_idx1, min_idx2:max_idx2] = QK[min_idx1: max_idx1, min_idx2:max_idx2]
+
+    return Output
+
 
 def torch_cal_select_mask(
     QK: torch.Tensor,
@@ -110,13 +375,134 @@ def torch_cal_select_mask(
                          ]
                         )
 def test_select_analysis_bool(param_list):
-    dtype, SEQ_LEN, indice = param_list
+    dtype, SEQ_LEN, index = param_list
     assert dtype == 'bool'
     qk_cal = torch.empty(SEQ_LEN).npu()
-    indices = torch.tensor([indice]).npu()
-    qk_ref = torch_cal_select_mask_bool(indice, SEQ_LEN, SEQ_LEN)
+    indices = torch.tensor([index]).npu()
+    qk_ref = torch_cal_select_mask_bool(index, SEQ_LEN, SEQ_LEN)
     kernel_cal_select_mask_bool[(1,)](
         qk_cal, indices, SEQ_LEN, SEQ_LEN
+    )
+    test_common.validate_cmp(dtype, qk_cal, qk_ref)
+
+
+@pytest.mark.parametrize('param_list',
+                         [
+                            ['float16', 64, True],
+                            ['float32', 64, False],
+                         ]
+                        )
+def test_select_analysis_one_dim_static_offset(param_list):
+    dtype, SEQ_LEN, start_zero = param_list
+    assert dtype != 'bool'
+    qk = torch.rand([SEQ_LEN], dtype=eval('torch.' + dtype), device='npu')
+    qk_cal = torch.empty_like(qk).npu()
+    other = torch.zeros_like(qk).npu()
+    qk_ref = torch_cal_select_mask_one_dim_static_offset(qk, other, start_zero)
+    kernel_cal_select_mask_one_dim_static_offset[(1,)](
+        qk, other, qk_cal, SEQ_LEN, start_zero
+    )
+    test_common.validate_cmp(dtype, qk_cal, qk_ref)
+
+
+@pytest.mark.parametrize('param_list',
+                         [
+                            ['float16', 64, 62, True],
+                            ['float32', 64, 62, False],
+                         ]
+                        )
+def test_select_analysis_one_dim_dyn_offset(param_list):
+    dtype, SEQ_LEN, index, start_zero = param_list
+    assert dtype != 'bool'
+    qk = torch.rand([SEQ_LEN], dtype=eval('torch.' + dtype), device='npu')
+    qk_cal = torch.empty_like(qk).npu()
+    other = torch.zeros_like(qk).npu()
+    indices_tensor = torch.tensor([index]).npu()
+    qk_ref = torch_cal_select_mask_one_dim_dyn_offset(qk, other, indices_tensor, start_zero)
+    kernel_cal_select_mask_one_dim_dyn_offset[(1,)](
+        qk, other, qk_cal, indices_tensor,
+        SEQ_LEN, start_zero
+    )
+    test_common.validate_cmp(dtype, qk_cal, qk_ref)
+
+
+@pytest.mark.parametrize('param_list',
+                         [
+                            ['float16', 64, 1, 62],
+                            ['float32', 64, 1, 62],
+                         ]
+                        )
+def test_select_analysis_one_dim_two_dyn_offset(param_list):
+    dtype, SEQ_LEN, minIndex, maxIndex = param_list
+    assert dtype != 'bool'
+    qk = torch.rand([SEQ_LEN], dtype=eval('torch.' + dtype), device='npu')
+    qk_cal = torch.empty_like(qk).npu()
+    other = torch.zeros_like(qk).npu()
+    indices_tensor = torch.tensor([minIndex, maxIndex]).npu()
+    qk_ref = torch_cal_select_mask_one_dim_two_dyn_offset(qk, other, indices_tensor)
+    kernel_cal_select_mask_one_dim_two_dyn_offset[(1,)](
+        qk, other, qk_cal, indices_tensor, SEQ_LEN
+    )
+    test_common.validate_cmp(dtype, qk_cal, qk_ref)
+
+
+@pytest.mark.parametrize('param_list',
+                         [
+                            ['float16', 64, True],
+                            ['float32', 64, False],
+                         ]
+                        )
+def test_select_analysis_two_dim_static_offset(param_list):
+    dtype, SEQ_LEN, start_zero = param_list
+    assert dtype != 'bool'
+    qk = torch.rand([SEQ_LEN, SEQ_LEN], dtype=eval('torch.' + dtype), device='npu')
+    qk_cal = torch.empty_like(qk).npu()
+    other = torch.zeros_like(qk).npu()
+    qk_ref = torch_cal_select_mask_two_dim_static_offset(qk, other, start_zero)
+    kernel_cal_select_mask_two_dim_static_offset[(1,)](
+        qk, other, qk_cal, SEQ_LEN, start_zero
+    )
+    test_common.validate_cmp(dtype, qk_cal, qk_ref)
+
+
+@pytest.mark.parametrize('param_list',
+                         [
+                            ['float16', 64, 1, 62, True],
+                            ['float32', 64, 1, 62, False],
+                         ]
+                        )
+def test_select_analysis_two_dim_one_dyn_offset(param_list):
+    dtype, SEQ_LEN, minIndex, maxIndex, start_zero = param_list
+    assert dtype != 'bool'
+
+    qk = torch.rand([SEQ_LEN, SEQ_LEN], dtype=eval('torch.' + dtype), device='npu')
+    qk_cal = torch.empty_like(qk).npu()
+    other = torch.zeros_like(qk).npu()
+    indices_tensor = torch.tensor([minIndex, maxIndex]).npu()
+    qk_ref = torch_cal_select_mask_two_dim_one_dyn_offset(qk, other, indices_tensor, start_zero)
+    kernel_cal_select_mask_two_dim_one_dyn_offset[(1,)](
+        qk, other, qk_cal, indices_tensor, SEQ_LEN, start_zero
+    )
+    test_common.validate_cmp(dtype, qk_cal, qk_ref)
+
+
+@pytest.mark.parametrize('param_list',
+                         [
+                            ['float16', 64, 2, 62, 1, 63],
+                            ['float32', 64, 2, 62, 1, 63],
+                         ]
+                        )
+def test_select_analysis_two_dim_all_dyn_offset(param_list):
+    dtype, SEQ_LEN, minIndex1, maxIndex1, minIndex2, maxIndex2 = param_list
+    assert dtype != 'bool'
+
+    qk = torch.rand([SEQ_LEN, SEQ_LEN], dtype=eval('torch.' + dtype), device='npu')
+    qk_cal = torch.empty_like(qk).npu()
+    other = torch.zeros_like(qk).npu()
+    indices_tensor = torch.tensor([minIndex1, maxIndex1, minIndex2, maxIndex2]).npu()
+    qk_ref = torch_cal_select_mask_two_dim_all_dyn_offset(qk, other, indices_tensor)
+    kernel_cal_select_mask_two_dim_all_dyn_offset[(1,)](
+        qk, other, qk_cal, indices_tensor, SEQ_LEN
     )
     test_common.validate_cmp(dtype, qk_cal, qk_ref)
 

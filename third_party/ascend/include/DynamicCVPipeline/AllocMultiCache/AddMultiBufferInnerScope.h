5a9d215701d1ff712472bde6b43adf339b6508ec
cxtverygood123 <chenxiaotian12@huawei.com>
2026-05-08 10:58:57 +0800

    initialization of inner scope

---
+++ third_party/ascend/include/DynamicCVPipeline/AllocMultiCache/AddMultiBufferInnerScope.h
@@ -0,0 +1,54 @@
+/*
+* Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+#ifndef TRITON_ADAPTER_ALLOC_MULTI_CACHE_ADD_MULTI_BUFFER_INNER_SCOPE_PASS_H
+#define TRITON_ADAPTER_ALLOC_MULTI_CACHE_ADD_MULTI_BUFFER_INNER_SCOPE_PASS_H
+
+#include "mlir/Pass/Pass.h"
+#include "mlir/IR/BuiltinOps.h"
+
+namespace mlir {
+namespace triton {
+
+// AddMultiBufferInnerScopePass for adding multi-buffer optimization within inner scope
+class AddMultiBufferInnerScopePass
+    : public PassWrapper<AddMultiBufferInnerScopePass, OperationPass<ModuleOp>> {
+public:
+    MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(AddMultiBufferInnerScopePass)
+
+    // Constructor
+    AddMultiBufferInnerScopePass() = default;
+
+    // Pass argument
+    StringRef getArgument() const override { return "add_multi_buffer_inner_scope"; }
+
+    // Run the pass
+    void runOnOperation() override;
+};
+
+// Create the pass
+std::unique_ptr<OperationPass<ModuleOp>> createAddMultiBufferInnerScopePass();
+
+} // namespace triton
+} // namespace mlir
+
+#endif // TRITON_ADAPTER_ALLOC_MULTI_CACHE_ADD_MULTI_BUFFER_INNER_SCOPE_PASS_H
